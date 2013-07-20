dce12beb124206e441685e58bcb720631783d7b4
tmatsukijp <matsuki@tokyo-kasei.ac.jp>
2013-07-21 00:56:09 +0900

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
+var buffer = Buffer(256);
+var len = buffer.write(readFileSync("index.html", "utf-8"));
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send(buffer.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
