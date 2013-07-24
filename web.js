b2b7b8733462d41e48e6543639714293f6735ee2
tmatsukijp <matsuki@tokyo-kasei.ac.jp>
2013-07-25 00:54:04 +0900

    Do from the biggining

--- web.js
+++ web.js
@@ -1,12 +1,9 @@
 var express = require('express');
 
-var buffer = Buffer(256);
-var len = buffer.write(fs.readFileSync("index.html", "utf-8"));
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf8', 0, len));
+  response.send('Hello World! 2');
 });
 
 var port = process.env.PORT || 5000;
