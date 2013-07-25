17e226e8e5914d93b2671a8f70a1bfd6956ad2c3
tmatsukijp <matsuki@tokyo-kasei.ac.jp>
2013-07-25 11:35:37 +0900

    Chenged web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var len = buffer.write(fs.readFileSync("index.html", "utf-8"));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf8', 0, len-1));
+  response.send(buffer.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
